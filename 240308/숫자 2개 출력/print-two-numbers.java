77e72f9acc3bc2bf09bdcc377cac07ec42c6f9a2
Dongwoo Lee <ys932184@gmail.com>
2024-03-08 11:22:28 +0900

    Title: 숫자 2개 출력, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240308/숫자 2개 출력/print-two-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+    StringBuilder builder = new StringBuilder();
+    builder.append(3);
+    builder.append(" ");
+    builder.append(5);
+    System.out.println(builder.toString());
+    }
+}
