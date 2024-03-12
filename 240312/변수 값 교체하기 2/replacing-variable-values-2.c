0d7b2f2ffabf533ccbd2fd9bba593da08705ab4f
Dongwoo Lee <ys932184@gmail.com>
2024-03-12 20:35:03 +0900

    Title: 변수 값 교체하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/변수 값 교체하기 2/replacing-variable-values-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5;
+    int b = 3;
+    a = b;
+    printf("%d", a);
+    printf("\n");
+    printf("%d", b);
+    return 0;
+}
