f34dcdaef71311c06356d74a7ef60aae0c3d0e9b
Dongwoo Lee <ys932184@gmail.com>
2024-03-14 23:01:54 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240314/정수 복사/copy-integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    int b = 4;
+    b = a;
+    printf("%d %d", a, b);
+    printf("\n");
+    printf("%d", a * b);
+    return 0;
+}
