c3d998d3816b67ce954a1ca4dc24a4e150739bb8
Dongwoo Lee <ys932184@gmail.com>
2024-04-22 21:01:31 +0900

    Title: 삼항연산자 2, Time: 211ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240422/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try (Scanner sc = new Scanner(System.in);){        
+            int a = sc.nextInt();
+            String result = a == 1 ? "t" : "f";
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
