9efba6f7bfd8761fb266851ff7b68cf8c26ce0f9
Dongwoo Lee <ys932184@gmail.com>
2024-04-23 22:07:59 +0900

    Title: 물의 상태, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240423/물의 상태/state-of-water.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int t0 = sc.nextInt();
+
+        if (t0 >= 100) {
+            System.out.println("water");
+        } else if (t0 < 0) {
+            System.out.println("ice");
+        } else {
+            System.out.println("water");   
+        }
+    }
+}
