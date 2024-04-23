5a7120fbdb129a4ce82a5f1c49c5441d40e9ff07
Dongwoo Lee <ys932184@gmail.com>
2024-04-23 22:14:27 +0900

    Title: 살 수 있는 물건, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240423/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try (Scanner sc = new Scanner(System.in);) {
+            int money = sc.nextInt();
+
+            if (money >= 3000) {
+                System.out.println("book");
+            } else if (money >= 1000) {
+                System.out.println("mask");
+            } else {
+                System.out.println("no");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
