17864bef5f3a371c273b74ff44aae21e1dd2fe03
Dongwoo Lee <ys932184@gmail.com>
2024-05-17 23:59:12 +0900

    Title: 범위 안의 수, Time: 258ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240517/범위 안의 수/number-in-range.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try (Scanner sc = new Scanner(System.in);) {
+            int a = sc.nextInt();
+            if (a >= 10 && a <= 20) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
