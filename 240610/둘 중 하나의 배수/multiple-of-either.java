3d381842159160030bec88c7aa9f89c87cb81882
Dongwoo Lee <ys932184@gmail.com>
2024-06-10 22:25:26 +0900

    Title: 둘 중 하나의 배수, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try(Scanner sc = new Scanner(System.in);) {
+            int a = sc.nextInt();
+
+            if (a % 3 == 0 || a % 5 == 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        } catch(Exception e) {
+            System.out.println("-1");
+        }
+    }
+}
