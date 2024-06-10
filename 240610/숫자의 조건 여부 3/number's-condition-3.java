0127526fc7b5de64235604a81a3a084139eb8f48
Dongwoo Lee <ys932184@gmail.com>
2024-06-10 22:23:46 +0900

    Title: 숫자의 조건 여부 3, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/숫자의 조건 여부 3/number's-condition-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try (Scanner sc = new Scanner(System.in);) {
+            int a = sc.nextInt();
+
+            if (a % 13 == 0 || a % 19 == 0) {
+                System.out.println("True");
+            } else {
+                System.out.println("False");
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+
+    }
+}
