60f4911c5d7fba7503d2bf42e15f007b641164d0
Dongwoo Lee <ys932184@gmail.com>
2024-07-25 08:42:51 +0900

    Title: 오름 내림차순 정렬, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240725/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        try(Scanner sc = new Scanner(System.in);) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            
+            System.out.println();
+            Integer[] arr1 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+            Arrays.sort(arr1, Collections.reverseOrder());
+              for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr1[i] + " ");
+            }
+        } catch (Exceptio e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+}
