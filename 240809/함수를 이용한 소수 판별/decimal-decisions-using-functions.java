4ec625b76c51ef96fb6cca465c6784ceca34079b
Dongwoo Lee <ys932184@gmail.com>
2024-08-09 08:44:47 +0900

    Title: 함수를 이용한 소수 판별, Time: 298ms, Memory: 16MB, Status: Passed - CodeTree

--- 240809/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240809/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -29,11 +29,15 @@ public class Main {
             }
         }
 
-        return  result;
+        return result;
 
     }
 
     public static boolean isPrime(int x) {
+        if (x == 1) {
+            return false;
+        }
+
         for (int i = 2; i < x; i++) {
             if (x % i == 0) {
                 return false;
