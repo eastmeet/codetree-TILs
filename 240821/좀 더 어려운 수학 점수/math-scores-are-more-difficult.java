bfc49acb94d1a1191ef6bbf10b299fe5015b97ea
Dongwoo Lee <ys932184@gmail.com>
2024-08-21 23:13:48 +0900

    Title: 좀 더 어려운 수학 점수, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240821/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 240821/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -4,26 +4,25 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         try (Scanner sc = new Scanner(System.in)) {
-            int mathA = sc.nextInt();
-            int engA = sc.nextInt();
-            int mathB = sc.nextInt();
-            int engB = sc.nextInt();
+            Integer mathA = sc.nextInt();
+            Integer engA = sc.nextInt();
+            Integer mathB = sc.nextInt();
+            Integer engB = sc.nextInt();
 
-            if (mathA > mathB) {
-                System.out.println("A");
-            }
-            
-            if (mathA < mathB) {
-              System.out.println("B");
-            }
-
-            if (mathA == mathB){
-               if (engA > engB) {
-                    System.out.println("A");
-                } else {
-                    System.out.println("B");
-                }
-            }
+            String result = compareStudentScore(mathA, engA, mathB,engB);
+            System.out.println(result);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            throw e;
         }
     }
+
+    public static String compareStudentScore(Integer mathA, Integer engA, Integer mathB, Integer engB) { 
+           int mathComparison = mathA.compareTo(mathB);
+           if (mathComparison != 0) {
+               return mathComparison > 0 ? "A" : "B"; 
+           }
+
+           return engA.compareTo(mathB) >= 0 ? "A" : "B";
+        }
 }
