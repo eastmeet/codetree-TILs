86e6fdcd0a97abd32801c59a40559c07500b7b63
Dongwoo Lee <ys932184@gmail.com>
2024-08-31 22:55:09 +0900

    Title: 증가시키며 출력하기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240831/증가시키며 출력하기/increase-and-print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for (int i = 5; i <= 17; i += 2) {
+            System.out.print(i);
+            System.out.print(" ");
+        }
+    }
+}
