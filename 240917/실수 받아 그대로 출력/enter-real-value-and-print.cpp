14d29e820a6c682ab36b41f68fe03ff367b3403d
Dongwoo Lee <ys932184@gmail.com>
2024-09-17 00:33:24 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 240917/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    // 변수 선언 및 입력
     double n;
     cin >> n;
-    double rounded = round(n * 100.0) / 100.0;
-    cout << fixed << setprecision(2) << rounded << endl;
+    
+    cout << fixed;
+    cout.precision(2);
+    cout << n;
     return 0;
 }
