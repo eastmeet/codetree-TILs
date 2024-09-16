d362a4f8155beba7322ec82a1eb5b4f962c41abd
Dongwoo Lee <ys932184@gmail.com>
2024-09-17 00:34:15 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 240917/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    // 변수 선언 및 입력
+    // 여기에 코드를 작성해주세요.
     double n;
     cin >> n;
-    
     cout << fixed;
     cout.precision(2);
     cout << n;
