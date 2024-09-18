a479a27eda24d8246c0fbbdb18d1e99a77010ff3
Dongwoo Lee <ys932184@gmail.com>
2024-09-18 23:19:32 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240918/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int yyyy, mm, dd;
+    char dash;
+    char dot = '.';
+    cin >> mm >> dash >> dd >> dash >> yyyy;
+    cout << yyyy << dot << mm << dot << dd << endl;
+    return 0;
+}
