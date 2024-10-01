fdb83c62d171a86ec95d434e3a24583bce887f6b
Dongwoo Lee <ys932184@gmail.com>
2024-10-01 21:48:30 +0900

    Title: 10개 입력받기, Time: 269ms, Memory: 16MB, Status: Passed - CodeTree

--- 241001/10개 입력받기/receive-10-inputs.java
+++ 241001/10개 입력받기/receive-10-inputs.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         try (Scanner sc = new Scanner(System.in)) { 
+            // 사용자 입력 
             int[] arr = new int[10];
             int cnt = 0;
             for (int i = 0; i < arr.length; i++) {
@@ -15,7 +16,9 @@ public class Main {
                 }
                 cnt++;
             }
-            int sum = Arrays.stream(arr).sum();
+            
+            // 합계, 평균 계산
+            int sum = Arrays.stream(arr).limit(cnt).sum();
             double avg = (double) sum / cnt;
             double roundedAvg = Math.round(avg * 10.0) / 10.0;
             System.out.print(sum + " " + roundedAvg);
