19c528acbf26e8cfffcaed1490f9651b30a3679a
Dongwoo Lee <ys932184@gmail.com>
2024-10-01 21:46:37 +0900

    Title: 10개 입력받기, Time: 233ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241001/10개 입력받기/receive-10-inputs.java
+++ 241001/10개 입력받기/receive-10-inputs.java
@@ -7,16 +7,15 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         try (Scanner sc = new Scanner(System.in)) { 
             int[] arr = new int[10];
+            int cnt = 1;
             for (int i = 0; i < arr.length; i++) {
                 arr[i] = sc.nextInt();
                 if (arr[i] == 0) {
                     break;
                 }
+                cnt++;
             }
             int sum = Arrays.stream(arr).sum();
-            double cnt = Arrays.stream(arr)
-                        .filter(num -> num != 0)
-                        .count();
             double avg = sum / cnt;
             double roundedAvg = Math.round(avg * 10.0) / 10.0;
             System.out.print(sum + " " + roundedAvg);
