a410e9a861e3a0a9e92103d42a6ba4618d1a0ff8
Dongwoo Lee <ys932184@gmail.com>
2025-02-10 21:57:43 +0900

    Title: 사칙연산 함수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250210/사칙연산 함수/quadratic-operations-function.java
+++ 250210/사칙연산 함수/quadratic-operations-function.java
@@ -3,32 +3,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        String o = sc.next();
-        int c = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            String o = sc.next();
+            int c = sc.nextInt();
 
-        String result = calculate(a, o, c);
-        if (result.equals("False")) {
-            System.out.println("False");
-            return;
-        } 
-        
-        System.out.println(a + " " + o + " " + c + " = " + result);
+            String result = calculate(a, o.charAt(0), c);
+            if (result.equals("False")) {
+                System.out.println("False");
+                return;
+            }
+            System.out.println(a + " " + o + " " + c + " = " + result);
+        } catch (ArithmeticException e) {
+                System.out.println("False");
+        }
     }
 
-    private static String calculate(int a, String o, int c) {
-        switch (o) {
-            case "+":
-                return String.valueOf(a + c);
-            case "-":
-                return String.valueOf(a - c);
-            case "*":
-                return String.valueOf(a * c);
-            case "/":
-                return String.valueOf(a / c);
-            default:
+    private static String calculate(int a, char operator, int c) {         
+        try {
+            int result;
+            switch (operator) {
+                case '+':
+                    result = a + c;
+                    break;
+                case '-':
+                    result = a - c;
+                    break;
+                case '*':
+                    result = a * c;
+                    break;
+                case '/':
+                    if (c == 0) {
+                        throw new ArithmeticException("Division by zero");
+                    }
+                    result = a / c;
+                    break;
+                default:
+                    return "False";
+            } 
+            return String.valueOf(result);
+            } catch (Exception e) {
                 return "False";
-        }
+            }
     }
 }
